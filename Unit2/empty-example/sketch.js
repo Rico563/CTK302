868e04a837932d3adde25d8c00123d944291d198
Rico563 <rmart13@ilstu.edu>
2021-02-23 00:26:45 -0600

    Great songs

--- Unit2/empty-example/sketch.js
+++ Unit2/empty-example/sketch.js
@@ -1,39 +1,7 @@
-let myState = 0;
-let myTimer = 0;
-
 function setup() {
   createCanvas(500, 500);
 }
 
 function draw() {
-  switch (myState) {
 
-    case 0:
-      background('silver');
-      myTimer++;
-      if (myTimer > 3 * 60) {
-        myTimer = 0;
-        myState = 1;
-      }
-      break;
-
-    case 1:
-      background('purple');
-      myTimer++;
-      if (myTimer > 1 * 60) {
-        myTimer = 0;
-        myState = 2;
-      }
-      break;
-
-    case 2:
-      background('orange');
-      myTimer++;
-      if (myTimer > 2 * 60) {
-        myTimer = 0;
-        myState = 0;
-      }
-      break;
-
-  }
 }
