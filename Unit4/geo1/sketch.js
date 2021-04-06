df71696ec2d0e3b11202ddffbcc4dcffc8cefa48
Rico563 <rmart13@ilstu.edu>
2021-04-06 15:09:37 -0500

    tyf

--- Unit4/geo1/sketch.js
+++ Unit4/geo1/sketch.js
@@ -1,33 +1,31 @@
-var num;
-var distance;
+var num ;
+var distance ;
 
 var locationData;
+function preload(){
 
-function preload() {
-
-  locationData = getCurrentPosition();
+    locationData =  getCurrentPosition();
 }
 
-function setup() {
-  num = 0;
-  intervalCurrentPosition(positionPing, 5000);
-  createCanvas(displayWidth, displayHeight);
+function setup(){
+	num = 0 ;
+    intervalCurrentPosition(positionPing, 5000) ;
+	createCanvas(displayWidth, displayHeight) ;
 }
 
 function draw() {
 
 
 }
+function positionPing(position){
+	textSize(24) ;
+	num++ ;
+	background(255) ;
+    text("lat: " + position.latitude, 10, 40);
+    text("long: " + position.longitude, 10, 90);
+	text("number of updates " + num, 10, 140) ;
 
-function positionPing(position) {
-  textSize(24);
-  num++;
-  background(255);
-  text("lat: " + position.latitude, 10, 40);
-  text("long: " + position.longitude, 10, 90);
-  text("number of updates " + num, 10, 140);
-
-  distance = calcGeoDistance(locationData.latitude, locationData.longitude, position.latitude, position.longitude, 'mi');
-  text("you have moved " + distance, 10, 190);
+	distance = calcGeoDistance(locationData.latitude, locationData.longitude, position.latitude, position.longitude, 'mi') ;
+	text("you have moved " + distance, 10, 190) ;
 
 }
